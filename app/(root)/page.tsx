3f63ea0e48cb3902a4e920e5b62c2cde5e82f0e5
ChristianAlteri <alteri.christian@gmail.com>
2024-02-06 18:35:06 +0000

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+    return (
+      <div >hello world
+        <Button variant={"default"} > Click </Button>
+      </div>
+    )
+  }
+  
